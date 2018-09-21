9671c124fcfc4a22dc01c5094a7ec6b2cb808fc3
Jardim, Adam <ajardim@ea.com>
2018-09-21 15:43:45 -0700

    Input guards, small fixes

--- generateSpawnDinoCode.py
+++ generateSpawnDinoCode.py
@@ -19,7 +19,7 @@ def makeSpawnLimitCommand(codeName, maxPerc):
     return "(NPCClassString=\"" + codeName + "\",MaxPercentageOfDesiredNumToAllow=" + str(maxPerc) + ")"
 
 def generateSpawnDinoCode(reg, listOfSpawnEntryCommands, listOfSpawnLimitCommands):
-    command = "ConfigAddNPCSpawnEntriesContainer=(NPCSpawnEntriesContainerClassString=\"" + reg + ",NPCSpawnEntries=("
+    command = "ConfigAddNPCSpawnEntriesContainer=(NPCSpawnEntriesContainerClassString=\"" + reg + "\",NPCSpawnEntries=("
 
     #add spawn entry commands
     for index in range(len(listOfSpawnEntryCommands)):
@@ -114,7 +114,18 @@ mapCounter = 0
 for map in listOfMaps:
     print("\t", mapCounter, " - ", map.getName())
     mapCounter+=1
-mapChoiceInput = int(input("Choose a map #: "))
+badMapInput = True
+while(badMapInput):
+    mapChoiceInputStr = input("Choose a map #: ")
+    if mapChoiceInputStr.isnumeric():
+        mapChoiceInput = int(mapChoiceInputStr)
+        if (mapChoiceInput >= 0) and (mapChoiceInput < len(listOfMaps)):
+            badMapInput = False
+        else:
+            print("ERROR: Map choice number must be one listed above!")
+    else:
+        print("ERROR: Map choice must be a number!")
+
 chosenMap = listOfMaps[mapChoiceInput]
 print("You chose : ", chosenMap.getName(), "\n")
 
@@ -124,12 +135,31 @@ regionCounter = 0
 for region in chosenMap.getListOfRegions():
     print("\t", regionCounter, " - ", region)
     regionCounter+=1
-regionChoiceInput = int(input("Choose a region #: "))
+badRegionInput = True
+while(badRegionInput):
+    regionChoiceInputStr = input("Choose a region #: ")
+    if (regionChoiceInputStr.isnumeric()):
+        regionChoiceInput = int(regionChoiceInputStr)
+        if regionChoiceInput >= 0 and regionChoiceInput < len(chosenMap.getListOfRegions()):
+            badRegionInput = False
+        else:
+            print("ERROR: Region choice number must be one listed above!")
+    else:
+        print("ERROR: Region choice must be a number!")
+
 chosenRegion = chosenMap.getListOfRegions()[regionChoiceInput]
 print("You chose : ", chosenRegion, "\n")
 
+
 #Choose number of dinos to add to region
-numDinosInput = int(input("Number of dinos to add to this region: "))
+badInput = True
+while(badInput):
+    numDinosInputStr = input("Number of dinos to add to this region: ")
+    if numDinosInputStr.isnumeric():
+        numDinosInput = int(numDinosInputStr)
+        badInput = False
+    else:
+        print("ERROR: Must be a positive whole number")
 
 #Choose dinos
 npcSpawnEntryCommands = list()
@@ -141,7 +171,17 @@ for dinoEntry in range(numDinosInput):
     for dino in listOfDinos:
         print("\t", dinoCounter, " - ", listOfDinos[dinoCounter].getName())
         dinoCounter+=1
-    chosenDinoInput = int(input("Which dino would you like to add? : "))
+    badDinoInput = True
+    while(badDinoInput):
+        chosenDinoInputStr = input("Choose a dino #: ")
+        if (chosenDinoInputStr.isnumeric()):
+            chosenDinoInput = int(chosenDinoInputStr)
+            if (chosenDinoInput >= 0) and (chosenDinoInput < len(listOfDinos)):
+                badDinoInput = False
+            else:
+                print("ERROR: Dino choice number must be one listed above!")
+        else:
+            print("ERROR: Dino choice must be a number!")
     chosenDino = listOfDinos[chosenDinoInput]
     print("You chose : ", chosenDino.getName())
 
@@ -151,16 +191,24 @@ for dinoEntry in range(numDinosInput):
     #choose entry weight
     entryWeight = 999
     while((entryWeight > 1) or (entryWeight < 0)):
-        entryWeight = float(input("Entry Weight: "))
-        if((entryWeight > 1) or (entryWeight < 0)):
-            print("ERROR: Entry Weight must be between 0 and 1")
+        entryWeightStr = input("Entry Weight: ")
+        if (entryWeightStr.replace('.','',1).isnumeric()):
+            entryWeight = float(entryWeightStr)
+            if((entryWeight > 1) or (entryWeight < 0)):
+                print("ERROR: Entry Weight must be between 0 and 1")
+        else:
+            print("Entry weight must be a number")
 
     #choose max percent
     maxPercNumToAllow = 999
     while((maxPercNumToAllow > 1) or (maxPercNumToAllow < 0)):
-        maxPercNumToAllow = float(input("MaxPercentageOfDesiredNumToAllow: "))
-        if((maxPercNumToAllow > 1) or (maxPercNumToAllow < 0)):
-            print("ERROR: MaxPercentageOfDesiredNumToAllow must be between 0 and 1")
+        maxPercNumToAllowStr = input("MaxPercentageOfDesiredNumToAllow: ")
+        if (maxPercNumToAllowStr.replace('.','',1).isnumeric()):
+            maxPercNumToAllow = float(maxPercNumToAllowStr)
+            if((maxPercNumToAllow > 1) or (maxPercNumToAllow < 0)):
+                print("ERROR: MaxPercentageOfDesiredNumToAllow must be between 0 and 1")
+        else:
+            print("Max percent must be a number")
 
     #form commands
     entryCommand = makeSpawnEntriesCommandDino(entryName, entryWeight, chosenDino)
